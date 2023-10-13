932e9504e11ea2cd2d56f371c9daf3c445d458f4
秦泽帆 <124950885+q931326@users.noreply.github.com>
2023-10-13 20:39:55 +0800

    Create stack.h

---
+++ stack.h
@@ -0,0 +1,45 @@
+#ifndef STACK_H
+#define STACK_H
+#include <vector>
+#include <cstddef>
+#include <utility>
+using std::move;
+using std::vector;
+
+
+template<class T>
+class stack {
+    typedef    T              value_type;
+    typedef    T& reference;
+    typedef const T& const_reference;
+public:
+
+    stack() : datas_() {}
+
+    ~stack() {}
+
+    void push(value_type value) {
+        datas_.push_back(move(value));
+    }
+
+    const_reference top() const {
+        return datas_.back();
+    }
+
+    void pop() {
+        datas_.pop_back();
+    }
+
+    size_t size() const {
+        return datas_.size();
+    }
+
+    bool empty() const {
+        return datas_.empty();
+    }
+
+private:
+    vector<value_type> datas_;
+};
+
+#endif // STACK_H
