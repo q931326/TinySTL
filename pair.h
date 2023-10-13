193859990cdc9a887c92e79b4d2752071f7f4936
秦泽帆 <124950885+q931326@users.noreply.github.com>
2023-10-13 20:39:24 +0800

    Create pair.h

---
+++ pair.h
@@ -0,0 +1,20 @@
+#ifndef PAIR_H
+#define PAIR_H
+#include <utility>
+
+namespace tinystl {
+    template<class Key, class Value>
+    struct pair {
+
+        pair() {}
+
+        pair(Key k, Value v) :
+            first(std::move(k)),
+            second(std::move(v)) {}
+
+        Key first;
+        Value second;
+    };
+
+}
+#endif // PAIR_H
